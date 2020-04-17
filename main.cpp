a493da62c80f1dc82c6c4b947249282ff59aa14d
andresgalvez <63518568+andresgalvez@users.noreply.github.com>
2020-04-17 12:05:19 -0500

    Main meni upocion 1
    opcion 1

--- main.cpp
+++ main.cpp
@@ -8,20 +8,34 @@ using namespace std;
 
 int main()
 {
-    Videojuego  vj1("12",14,"62","61","73",62);
-    Persona  p1("12","62","61","73","62");
-    Empleado e1(5,"421","abc","null","61","t24","2t4","Q3HG");
-    Cliente c1("2ct",23,"c2","2c","v3y4","c4t2","52g");
 
-    Cliente *ptrC1 = &c1;
-    Empleado * ptrE1 = &e1;
-    Factura f1(13,"c34","c43","t2c","c2t",315,ptrC1, ptrE1);
-    vj1.toString();
-    p1.toString();
-    f1.toString();
+    Empleado *empleado;
+        Empleado *listaEmpleado[3];
+        empleado = new Empleado(300,"hola","hola","hola","hola","hola","hola","hola");
+        listaEmpleado[0] = empleado;
+        empleado = new Empleado(400,"ahola","ahola","ahola","ahola","ahola","ahola","ahola");
+        listaEmpleado[1] = empleado;
+        empleado = new Empleado(400,"bhola","bhola","bhola","bhola","bhola","bhola","bhola");
+        listaEmpleado[2] = empleado;
 
-    e1.Persona::toString();
+    int opcion;
+    do{
+        cout<<"MENU"<<endl;
+        cout<<"0. salir"<<endl;
+        cout<<"1. LISTAR EMPLEADOS"<<endl;
+        cout<<"2. LISTAR CLIENTES"<<endl;
+        cout<<"3. LISTAR FACTURA"<<endl;
+        cout<<"4. LISTAR PERSONA"<<endl;
+        cout<<"5. LISTAR VIDEOJUEGO"<<endl;
+        cin>>opcion;
+        switch(opcion){
+            case 1:
+                for(int i = 0; i < 3; i ++){
+                    listaEmpleado[i]->toString();
+                    };
+       };
 
+    }while(opcion !=0);
     cout << "Hello World!" << endl;
 
     return 0;
